24e884da390ba177c5f65d4cac61ed61c02740d1
Pakshal Shah <110169361+PakshalS@users.noreply.github.com>
2025-02-15 00:02:25 +0530

    Last changes

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,25 +1,30 @@
+import os
 from fastapi import FastAPI
+from services.rag_service import embed_json_data
 from api.chatbot import router as chatbot_router
 from fastapi.middleware.cors import CORSMiddleware
-from services.rag_service import embed_json_data
 
 app = FastAPI(title="RAG Chatbot API")
 
-# Enable CORS for Flutter frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Allow all origins (Change for security)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Ensure FAISS is ready
-embed_json_data()
-
 # Include chatbot routes
 app.include_router(chatbot_router)
+
+# Ensure FAISS is ready
+embed_json_data()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the RAG Chatbot API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    PORT = int(os.getenv("PORT", 8000))  # Render provides PORT env variable
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
